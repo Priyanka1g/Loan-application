baaa69b5fc8b9bcf65312e10d5baaf89393acf10
Sachin <sachin@myself.com>
2022-02-18 16:00:31 +0530

    Application form added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Login from "./components/login/Login";
 import Register from "./components/login/Register";
+import ApplicationForm from "./components/user/ApplicationForm";
 function App() {
   return (
     <div className="App">
@@ -9,7 +10,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Login></Login>}/>
         <Route exact path="/register" element={<Register></Register>}/>
-
+        <Route exact path="/apply" element={<ApplicationForm/>}/>
       </Routes>
     </BrowserRouter>
     </div>
