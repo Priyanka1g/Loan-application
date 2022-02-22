799cc010564fc49c1d989b4b6dab460cdeaf7e02
Sachin <sachin@myself.com>
2022-02-22 15:54:15 +0530

    navbar style changed

--- src/components/admin/NavbarElements.js
+++ src/components/admin/NavbarElements.js
@@ -3,11 +3,12 @@ import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Nav = styled.nav`
-  background: #d2d8ff;
-  height: 70px;
+  background-image: url("https://www.transparenttextures.com/patterns/beige-paper.png");
+  background-color: white;
+  border-radius: 10px 10px 10px 10px;
+  height: 57px;
   display: flex;
-  justify-content: space-between;
-  padding: 0.5rem calc((100vw - 1200px) / 2);
+  padding: 0.5rem ;
   z-index: 10;
 `;
 
@@ -16,10 +17,11 @@ export const NavLink = styled(Link)`
   display: flex;
   align-items: center;
   text-decoration: none;
+  font-size: 17px;
+  font-weight: bold;
   padding: 0 4rem;
   height: 100%;
   cursor: pointer;
-
   &.active {
     color: #000000;
   }
