3670d16ae90c67f2ee3a77ecf71b05f5bf844f99
Sachin <sachin@myself.com>
2022-02-27 03:16:55 +0530

    validation of form

--- src/components/user/NavElements.jsx
+++ src/components/user/NavElements.jsx
@@ -3,23 +3,27 @@ import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Nav = styled.nav`
-  background: #fff;
-  height: 80px;
+  background-image: url("https://www.transparenttextures.com/patterns/beige-paper.png");
+  background-color: white;
+  border-radius: 10px 10px 10px 10px;
+  height: 57px;
   display: flex;
-  justify-content: space-between;
-  padding: 0.5rem calc((100vw - 1000px) / 2);
+  padding: 0.5rem 0.5rem;
+  margin:5px;
   z-index: 10;
 `;
 
 export const NavLink = styled(Link)`
   color: #000000;
   display: flex;
+  white-space: nowrap;
   align-items: center;
   text-decoration: none;
-  padding: 0 1rem;
+  font-size: 17px;
+  font-weight: bold;
+  padding: 0 2rem;
   height: 100%;
   cursor: pointer;
-
   &:hover {
     transition: all 0.2s ease-in-out;
     color: #082f58;
@@ -28,20 +32,29 @@ export const NavLink = styled(Link)`
     color:#082f58;
   }
 `;
-
 export const Bars = styled(FaBars)`
   display: none;
-  color: #000000;
+  color: #fff;
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
 `;
-
 export const NavMenu = styled.div`
   display: flex;
   align-items: center;
-  margin-right: -24px;
+  margin:0px 295px;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
-
 export const NavBtn = styled.nav`
   display: flex;
   align-items: center;
-  margin-right: 24px;
+  margin-right: 78px;
 `;
